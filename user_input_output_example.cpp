77b48d320d36bfac6f08653c3b7aeabced8aaba1
iamtejasv <tejasv0804@gmail.com>
2024-10-10 08:05:20 +0530

    add user input & output

---
+++ user_input_output_example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  // Standard I/O library
+// #include <bits/stdc++.h> // this includes all standard libraries in c++
+using namespace std;
+
+int main() {
+    // Output
+    cout << "hello tejas" << endl;
+    
+    // Taking input
+    int x, y;
+    cout << "Enter two integers: ";
+    cin >> x >> y; // Input two integers
+    cout << "Value of x is " << x << " and y is " << y << endl;
+
+    return 0;
+}
