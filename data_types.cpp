0953a0fd1fdf22c40705d510e67b2fadfb69df68
iamtejasv <tejasv0804@gmail.com>
2024-10-10 14:18:31 +0530

    add data types

---
+++ data_types.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 10; 
+    long y;
+    cin >> y;
+    long long z = 2000000000000000000;
+    float a = 5.6256;
+    double b;
+    cin >> b;
+    cout << "value of b is " << b;
+    return 0;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1 << " " << s2;
+    return 0;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str); 
+    cout << str;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+    cout << ch;
+    return 0;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch = 's';
+    cout << ch;
+}
+
+/*
+int -10^9 to 10^9
+long -10^12 to 10^12
+long long -10^18 to 10^18
+*/
