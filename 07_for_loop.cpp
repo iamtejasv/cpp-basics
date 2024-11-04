e929e09fc319a0b3612c4ecb4e79b6641c057a7f
iamtejasv <tejasv0804@gmail.com>
2024-11-04 08:23:21 +0530

    add for loop

---
+++ 07_for_loop.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int i;
+   for(i = 1; i <= 25; i = i + 5 ){
+      cout << "Tejas V " << i << endl;
+   }
+   cout << i << endl;
+   return 0;
+}
